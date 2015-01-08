1ee57201a5fff23b78fb97ecce0da9729d5df44c
sco_tt <scott@scottpinkelman.com>
2015-01-08 11:29:28 -0500

    linting tags.jz

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -1,32 +1,30 @@
-function initialize() {
+jQuery(document).ready(function($){
+  "use strict";
+
+  function queryFt() {
   var query = "SELECT 'Age', 'Tag1', 'Tag2', 'Tag3', 'Tag4' FROM " +
   "1D7abkHy7QdnOtPkRhDCENIMskH-ujJVXE7UsEceq ORDER BY 'Age'";
   var encodedQuery = encodeURIComponent(query);
 
   // Construct the URL
-  var url = ['https://www.googleapis.com/fusiontables/v2/query'];
-  url.push('?sql=' + encodedQuery);
-  url.push('&key=AIzaSyAm9yWCV7JPCTHCJut8whOjARd7pwROFDQ');
-  var queryURL = url.join('');
-  var queryLink = document.getElementById('query');
-  queryLink.innerHTML = "<a href=" + queryURL + ">" + queryURL + "</a>";  
-    url.push('&callback=?');
+  var url = ["https://www.googleapis.com/fusiontables/v2/query"];
+  url.push("?sql=" + encodedQuery);
+  url.push("&key=AIzaSyAm9yWCV7JPCTHCJut8whOjARd7pwROFDQ");
+  url.push("&callback=?");
     $("#ft-data").addClass("spinning");
 
-
-  // Send the JSONP request using jQuery
   $.ajax({
-    url: url.join(''),
-    dataType: 'jsonp',
+    url: url.join(""),
+    dataType: "jsonp",
     success: function (data) {
        uppercase(data);
     } 
   }); 
-  }
+}
 
 function uppercase (data) {
-  var data_uppercase = Array();
-  for (i=0, len = data.rows.length; i < len; i++) {
+  var data_uppercase = [];
+  for (var i = 0, len = data.rows.length; i < len; i++) {
     var row = data.rows[i];
     data_uppercase.push([row[0], 
                         row[1].toUpperCase(),
@@ -41,35 +39,35 @@ function uppercase (data) {
 
 function search (data_uppercase) {
    var tagList = [
-                    ["SNW!",0],
-                    ["EML!",0],
-                    ["ADO!",0], 
-                    ["APP!",0],
-                    ["BAS!",0],
-                    ["COV!",0],
-                    ["CRT!",0],
-                    ["DOC!",0],
-                    ["FRM!",0],
-                    ["FAX!",0],
-                    ["GAM!",0],
-                    ["HED!",0],
-                    ["HW!", 0],
-                    ["INT!",0],
-                    ["JOB!",0],
-                    ["JSE!",0],
-                    ["LC!", 0],
-                    ["PRT!",0],
-                    ["RES!",0],
-                    ["SRC!",0],
-                    ["ODB!",0],
-                    ["GDT!",0],
-                    ["ENT!",0]
+                    ["SNW!","Social Networking",0],  
+                    ["EML!","Email",0],
+                    ["ADO!","Technology Adoption",0], 
+                    ["APP!","Job Application",0],
+                    ["BAS!","Computer Basics",0],
+                    ["COV!","Cover Letter",0],
+                    ["CRT!","Creative/Artistic",0],
+                    ["DOC!","Document/Word processing",0],
+                    ["FRM!","Online forms",0],
+                    ["FAX!","Fax",0],
+                    ["GAM!","Games",0],
+                    ["HED!","Higher Education",0],
+                    ["HW!","Homework", 0],
+                    ["INT!","Got a job interview",0],
+                    ["JOB!","Got a job",0],
+                    ["JSE!","Job Search",0],
+                    ["LC!","Sign up for library card", 0],
+                    ["PRT!","Printing",0],
+                    ["RES!","Resume",0],
+                    ["SRC!","Internet Search",0],
+                    ["ODB!","Online Database",0],
+                    ["GDT!","Used Gadget",0],
+                    ["ENT!","Entrepreneurial Activity",0]
                   ];
 
   var totalVisits = 0;
   var taggedVisits = 0;
 
-  for (i=0, len = data_uppercase.length; i < len; i++) {
+  for (var i = 0, len = data_uppercase.length; i < len; i++) {
       totalVisits++;
       var arr = data_uppercase[i];
      
@@ -78,28 +76,48 @@ function search (data_uppercase) {
         taggedVisits++;
       }
       
-      //Test each row against our index. TO DO: make sure this is checking all parts of the arr array
+      //Test each row against our index and write to the array, acting as a count
       for (var j=0, len2 = tagList.length; j < len2; j++) {
           if (arr.indexOf(tagList[j][0]) != -1) {
-              tagList[j][1]++;
+              tagList[j][2]++;
           }
       }
   }
-  console.log(tagList);
-  console.log("Total Visits: " + totalVisits);
-  console.log("Tagged Visits: " + taggedVisits);
-
   writeData(tagList, totalVisits, taggedVisits);
 
 }
 
+
 function writeData(tagList, totalVisits, taggedVisits) {
-  console.log(tagList);
-  var ftdata = ['<table class="table table-striped"><thead><tr><th>Tag</th><th>Count</th</tr>'];
-    for (var i = 0, len = tagList.length; i < len; i++) {
-      ftdata.push('<tr><td>' + tagList[i][0] + '</td><td>' + tagList[i][1] + '</td></tr>');
-    }
-    ftdata.push('</tbody></table>');
-      $("#ft-data").removeClass("spinning");
-    document.getElementById('ft-data').innerHTML = ftdata.join('');
+  
+
+  function sortTaglist () {
+    tagList.sort(function (a, b) {
+      if (b[2] > a[2]) {
+        return 1;
+      }
+      if (b[2] < a[2]) {
+        return -1;
+      }
+      return 0;
+    });
   }
+
+  sortTaglist();
+
+  var ftdata = ["<table class='table table-striped'><thead><tr><th>Tag</th><th>Description</th><th>Count</th><th>%</th></tr>"];
+    for (var i = 0, len = tagList.length; i < len; i++) {
+      var percent = ((tagList[i][2]/taggedVisits)*100).toFixed(2);
+      ftdata.push("<tr>"+
+                    "<td>" + tagList[i][0] + "</td>" +
+                    "<td>" + tagList[i][1] + "</td>" +
+                    "<td>" + tagList[i][2] + "</td>" +
+                    "<td>" + percent + "</td>" +
+                  "</tr>");
+    }
+    ftdata.push("</tbody></table>");
+      $("#ft-data").removeClass("spinning");
+    document.getElementById("ft-data").innerHTML = ftdata.join("");
+  }
+    queryFt();
+});
